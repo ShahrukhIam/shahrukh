709180263a773dac6e465446829bcc5743470268
shahrukh <shahrukh981132@gmail.com>
2024-07-24 16:16:46 +0530

    about page

---
+++ about/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from "next/image";
+
+const about = () => {
+  return (
+    <div><h1>hello</h1>
+      <Image src="/demo.jpg"  width="500" height="300"></Image>
+    </div>
+  )
+}
+
+export default about
